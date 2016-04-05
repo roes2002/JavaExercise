c0ca18a38678854f16e3dfa2d877157594729b54
roes2002 <happy961004@naver.com>
2016-04-05 14:04:48 +0900

    Update Lake10.java

--- src/classes/Lake10.java
+++ src/classes/Lake10.java
@@ -1,45 +1,77 @@
-package classes;
-
 import java.util.Scanner;
 
 /**
- * Created by jyheo on 2016-04-04.
+ * Created by jyheo on 2016-04-03.
  */
-class FoolFish extends Fish {
-    FoolFish(String name, String shape) {
-        super(name, shape);
+class Fish {
+    private String name;
+    private String shape;
+    private int x, y;
+
+    public Fish(String name, String shape) {
+        this.name = name;
+        this.shape = shape;
+        x = 0;
+        y = 0;
+    }
+
+    public Fish() {
+        this.name = "Unknown";
+        this.shape = "<--<";
+        x = 0;
+        y = 0;
     }
 
     public void move(int width, int height) {
-        x++;
-        y++;
+        double rand = Math.random();
+        this.x = (int) (rand * 80);
+        this.y = (int) (rand * 20);
+
         if (x >= width)
             x = 0;
         if (y >= height)
             y = 0;
     }
+
+    public void move2(int width, int height) {
+        x++;
+        y++;
+
+        if (x >= width)
+            x = 0;
+        if (y >= height)
+            y = 0;
+
+    }
+
+    public void display(int x, int y) {
+        if (this.x == x && this.y == y) {
+            System.out.print(shape);
+        }
+    }
 }
 
-public class Lake10 {
+public class Lake {
     private int width;
     private int height;
-    private Fish[] fishes;
+    private Fish fishlist[] = new Fish[10];
 
-    public Lake10(int width, int height) {
+    public Lake(int width, int height) {
         this.width = width;
         this.height = height;
-        fishes = new Fish[10];
-        for (int i = 0; i < fishes.length; i++) {
-            if (i % 2 == 0)
-                fishes[i] = new Fish("Even", "<#--<");
-            else
-                fishes[i] = new FoolFish("Odd", "<$--<");
+        for (int i = 0; i < 10; i++) {
+            fishlist[i] = new Fish();
         }
     }
 
     public void moveFish() {
-        for (Fish f : fishes)
-            f.move(width, height);
+        for (int i = 0; i < fishlist.length; i++) {
+            if (i % 2 == 0) {
+                fishlist[i].move2(width, height);
+            } else {
+                fishlist[i].move(width, height);
+            }
+        }
     }
 
     public void display() {
@@ -49,8 +81,9 @@ public class Lake10 {
         for (int i = 0; i < height; i++) {
             System.out.print("|");
             for (int j = 0; j < width; j++) {
-                for (Fish f : fishes)
-                    f.display(j, i);
+                for (int k = 0; k < 10; k++) {
+                    fishlist[k].display(j, i);
+                }
                 System.out.print(" ");
             }
             System.out.println("|");
@@ -61,7 +94,7 @@ public class Lake10 {
     }
 
     public static void main(String args[]) {
-        Lake10 lake = new Lake10(80, 20);
+        Lake lake = new Lake(80, 20);
         Scanner scanner = new Scanner(System.in);
         while (true) {
             lake.moveFish();
