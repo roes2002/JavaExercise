4f98a6c19ff3f871ada818d0fbe5c3abc1ca5a8c
roes2002 <happy961004@naver.com>
2016-04-05 14:06:47 +0900

    Update Rectangle.java

--- src/classes/Rectangle.java
+++ src/classes/Rectangle.java
@@ -1,7 +1,15 @@
-package classes;
-
 public class Rectangle {
     private int x1, y1, x2, y2;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
 
     public void setPoints(int _x1, int _y1, int _x2, int _y2) {
         // TODO, check if x, y > 0
@@ -12,32 +20,69 @@ public class Rectangle {
     }
 
     public int getWidth() {
-        // TODO
-        return 0;
+        int lineX;
+        if (x1 > x2) {
+            lineX = x1 - x2;
+        } else if (x1 < x2) {
+            lineX = x2 - x1;
+        } else
+            lineX = 0;
+
+
+        return lineX;
     }
 
     public int getHeight() {
-        // TODO
-        return 0;
+        int lineY;
+        if (y1 > y2) {
+            lineY = y1 - y2;
+        } else if (y1 < y2) {
+            lineY = y2 - y1;
+        } else
+            lineY = 0;
+
+        return lineY;
     }
 
     public int getArea() {
-        // TODO
-        return 0;
+        return getWidth() * getHeight();
     }
 
     public boolean equals(Rectangle r) {
-        // TODO
-        return false;
+        if (x1 == r.x1 && x2 == r.x2 && y1 == r.y1 && y2 == r.y2)
+            return true;
+        else if(x1 == r.x2 && x2 == r.x1 && y1 == r.y2 && y2 == r.y1)
+            return true;
+
+
+            return false;
     }
 
+
     public boolean in(Rectangle r) {
-        // TODO
+        if(x1<x2 && y1<y2){
+            if(x1<=r.x1 && r.x1 <= x2 || y1<=r.y1 && y2<=r.y2) {
+                return true;
+            }
+        }
+        else if (x1>x2 && y1>y2){
+            if(r.x2>=x2 && r.y2>=y2 || x1>=r.x2 && y1 >=+ r.y1){
+                return true;
+            }
+        }
         return false;
     }
 
     public boolean overlap(Rectangle r) {
-        // TODO
+        if(x1<x2 && y1<y2){
+            if(x1<r.x1 || y1<r.y1)
+                return true;
+        }
+        else if(x1>x2 && y1>y2){
+            if(x2<r.x2 || y2<r.y2){
+                return true;
+            }
+        }
         return false;
     }
 
